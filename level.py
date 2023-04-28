8415ce317ec7cecb24d5aba6baf65dccb03cd58e
unknown <shawtrevor97@yahoo.com>
2023-04-28 10:50:37 -0700

    Added new map

--- level.py
+++ level.py
@@ -23,7 +23,7 @@ class Level:
 
     def create_map(self):
         # looks at each value in world map 
-        for row_index, row in enumerate(WORLD_MAP):
+        """ for row_index, row in enumerate(WORLD_MAP):
             for column_index, column in enumerate(row):
                 x = column_index * TILESIZE
                 y = row_index * TILESIZE
@@ -33,7 +33,9 @@ class Level:
                     Tile((x,y), [self.visible_sprites, self.obstacle_sprites])
 
                 if column == 'p':
-                    self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
+                    self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites """
+
+        self.player = Player((2000,1430), [self.visible_sprites], self.obstacle_sprites)
 
 
     def run(self):
@@ -53,11 +55,21 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+
+        # creating the floor 
+        self.floor_surface = pygame.image.load('graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surface.get_rect(topleft = (0,0))
+
     def custom_draw(self, player):
 
         #getting the offset 
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+
+        # drawing the floow 
+        floor_offset_pos = self.floor_rect.topleft - self.offset 
+        self.display_surface.blit(self.floor_surface, floor_offset_pos)
 
         #for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
